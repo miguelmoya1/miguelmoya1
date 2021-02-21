4f80e6ec7a6430c2db2b38aaa2018d793b590a26
Miguelmo <miguelmoyaortega@gmail.com>
2021-02-21 23:22:08 +0100

    redraw background

--- frontend/src/app/background/background.component.ts
+++ frontend/src/app/background/background.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 type Box = {
   size?: number | string;
@@ -12,24 +12,47 @@ type Box = {
   templateUrl: './background.component.html',
   styleUrls: ['./background.component.scss'],
 })
-export class BackgroundComponent {
-  public boxes: Box[] = [];
+export class BackgroundComponent implements OnInit {
+  public boxes: Box[] = new Array(20).fill({});
+  private lastWindowWidth?: number;
 
-  getRandom(min: number, max: number) {
+  constructor() {}
+
+  private getRandom(min: number, max: number) {
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
-  constructor() {
-    const a: any[] = new Array(20).fill({});
-    this.boxes = a.map((_) => {
-      const data: Box = {};
-      const size = this.getRandom(25, 150);
-      data.left = `${this.getRandom(size, window.innerWidth - size)}px`;
-      data.size = `${size}px`;
-      data.speedTop = this.getRandom(10, 50);
-      data.speedRotate = this.getRandom(10, 50);
-      return data;
-    });
-    console.log(this.boxes);
+  ngOnInit() {
+    this.setBoxes();
+    window.addEventListener(
+      'resize',
+      this.debounce(() => {
+        this.setBoxes();
+      })
+    );
+  }
+
+  private setBoxes() {
+    if (this.lastWindowWidth !== window.innerWidth) {
+      this.lastWindowWidth = window.innerWidth;
+
+      this.boxes = this.boxes.map((_) => {
+        const data: Box = {};
+        const size = this.getRandom(25, 150);
+        data.left = `${this.getRandom(size, window.innerWidth - size)}px`;
+        data.size = `${size}px`;
+        data.speedTop = this.getRandom(10, 50);
+        data.speedRotate = this.getRandom(10, 50);
+        return data;
+      });
+    }
+  }
+
+  private debounce(func: Function) {
+    let timer: any;
+    return (event: any) => {
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(func, 500, event);
+    };
   }
 }
