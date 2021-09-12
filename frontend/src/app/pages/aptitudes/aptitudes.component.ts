ced7abae38e9058eeb6ae357f2ce319ed7fa0e5b
Miguelmo <miguelmoyaortega@gmail.com>
2021-09-13 00:11:50 +0200

    style

--- frontend/src/app/pages/aptitudes/aptitudes.component.ts
+++ frontend/src/app/pages/aptitudes/aptitudes.component.ts
@@ -14,9 +14,14 @@ type Aptitude = {
       <app-title [title]="'Frameworks'"></app-title>
 
       <ng-container *ngFor="let aptitude of aptitudes">
-        <div class="aptitude" *ngFor="let data of aptitude.data">
+        <div
+          class="text-transparent hover:text-blue-500 flex flex-col items-center"
+          *ngFor="let data of aptitude.data"
+        >
           <app-svg [type]="data.value"></app-svg>
-          <h2 class="title">{{ data.title }}</h2>
+          <h2 class="duration-500 font-bold text-xl mt-4 mb-8">
+            {{ data.title }}
+          </h2>
         </div>
       </ng-container>
     </app-page>
