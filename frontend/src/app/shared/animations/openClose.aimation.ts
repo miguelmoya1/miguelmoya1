53ca0acfcb616cc36a9f3e29bd66c0540acfe6a5
Miguelmo <miguelmoyaortega@gmail.com>
2023-10-12 17:48:03 +0200

    Only web

--- frontend/src/app/shared/animations/openClose.aimation.ts
+++
@@ -1,8 +0,0 @@
-import { trigger, style, transition, animate } from '@angular/animations';
-
-export const openClose = trigger('openClose', [
-  transition('* <=> *', [
-    style({ transform: 'scaleX(0)', 'transform-origin': 'left' }),
-    animate('400ms ease', style({ transform: 'scaleX(1)' })),
-  ]),
-]);
