bb5b11e2be9e10cb050b4f40c8ed51afa4acfa07
Miguelmo <miguelmoyaortega@gmail.com>
2024-06-26 19:56:13 +0200

    feat: :art: styles

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Timeline from './components/timeline';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className='flex min-h-screen flex-col items-center justify-between p-6 lg:p-24'>
       <Header />
 
       <AboutMe />
