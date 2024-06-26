12062d98ffa009b541405e7d695199bc574eaecc
Miguelmo <miguelmoyaortega@gmail.com>
2024-06-26 09:04:41 +0200

    Update formatTextToElements function to fix last element missing period bug

--- app/utils/formatTextToElements.tsx
+++ app/utils/formatTextToElements.tsx
@@ -1,11 +1,15 @@
 export const formatTextToElements = (text: string) => {
-  return text
-    .split('. ')
-    .filter(Boolean)
-    .map((p, i) => (
-      <>
-        <p key={i}>{p}.</p>
-        <br />
-      </>
-    ));
+  return (
+    text
+      .split('. ')
+      .filter(Boolean)
+      // Remove the last . from the last element
+      .map((p, i, arr) => (i === arr.length - 1 ? p : `${p}.`))
+      .map((p, i) => (
+        <>
+          <p key={i}>{p}.</p>
+          <br />
+        </>
+      ))
+  );
 };
