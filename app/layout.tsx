45dfe2eaaea8b100576c5937ae097055f3d2900e
Miguelmo <miguelmoyaortega@gmail.com>
2024-06-26 21:04:41 +0200

    feat: update metadata keywords and Twitter card information

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,21 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Miguelmo - Full Stack Developer',
   description: 'Miguel Moya Ortega - Full Stack Developer',
+  keywords: [
+    'Miguel Moya Ortega',
+    'Full Stack Developer',
+    'Software Engineer',
+    'Web Developer',
+    'miguelmo',
+    'miguelmo.dev',
+  ],
+  robots: 'index, follow',
+  viewport: 'width=device-width, initial-scale=1.0',
+  twitter: {
+    card: 'summary',
+    site: '@miguelmo',
+    creator: '@miguelmo',
+  },
 };
 
 export default function RootLayout({
